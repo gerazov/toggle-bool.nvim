40efa3891470db83f72f88a157adb63fd6efa5b8
Ricardo Casía <ricardocasia.dev@gmail.com>
2023-05-20 18:53:43 +0200

    refactor: separated search and write steps

--- lua/toggle-bool/init.lua
+++ lua/toggle-bool/init.lua
@@ -7,20 +7,32 @@ M.conf = {
   },
 }
 
+local function find_toggle_word(sub_line)
+  local found_word, substitute_word
+  for key, value in pairs(M.conf.toggles) do
+    if string.find(sub_line, key) then
+      found_word = key
+      substitute_word = value
+    elseif string.find(sub_line, value) then
+      found_word = value
+      substitute_word = key
+    end
+  end
+
+  return found_word, substitute_word
+end
+
 M.toggle_bool = function()
   local line = vim.api.nvim_get_current_line()
   local _, col = unpack(vim.api.nvim_win_get_cursor(0))
   local sub_line = string.sub(line, col + 1)
-  for key, value in pairs(M.conf.toggles) do
-    if string.find(sub_line, key) then
-      local new_sub_line = string.gsub(sub_line, key, value, 1)
-      local new_line = string.sub(line, 1, col) .. new_sub_line
-      vim.api.nvim_set_current_line(new_line)
-    elseif string.find(sub_line, value) then
-      local new_sub_line = string.gsub(sub_line, value, key, 1)
-      local new_line = string.sub(line, 1, col) .. new_sub_line
-      vim.api.nvim_set_current_line(new_line)
-    end
+
+  local found_word, substitute_word = find_toggle_word(sub_line)
+
+  if(found_word) then
+    local new_sub_line = string.gsub(sub_line, found_word, substitute_word, 1)
+    local new_line = string.sub(line, 1, col) .. new_sub_line
+    vim.api.nvim_set_current_line(new_line)
   end
 end
 
